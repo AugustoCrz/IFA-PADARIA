67191f48a59c746a11030f2021ca73f503a29af6
AugustoCrz <augusto2308@hotmail.com>
2022-12-17 11:01:30 -0300

    first commit

---
+++ app-api/src/database/observacao.js
@@ -0,0 +1,17 @@
+//Criar a rota apagar no Node.js
+//app.delete("/artigo/:id", (req, res) => {
+	//Apagar o registro no banco de dados MongoDB
+//    const artigo = Artigo.deleteOne({_id: req.params.id}, (err) => {
+		//Retornar erro quando não conseguir apagar no banco de dados
+//        if(err) return res.status(400).json({
+//            error: true,
+//            message: "Error: Artigo não foi apagado com sucesso!"
+//        });
+
+		//Retornar mensagem de sucesso quando excluir o registro com sucesso no banco de dados
+//        return res.json({
+//            error: false,
+//            message: "Artigo apagado com sucesso!"
+//       });
+//    });
+//});
