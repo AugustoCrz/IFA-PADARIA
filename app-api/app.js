5dc4342598ae5fec47e505c778f365e711351324
AugustoCrz <augusto2308@hotmail.com>
2022-12-17 11:09:29 -0300

    api e auth

--- app-api/app.js
+++ app-api/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const ProdutoRoutes = require("./src/api/routes/ProdutoRoute")
 
 const app = express();
 
 //Configuração dos middlewares
-app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(bodyParser.json())
 
-//Configuração das rotas da API
-app.get("/", (req, res) =>{
-    res.send("Padaria- \nIfa- \nAugusto - \nBP:3008444");
-    res.end();
-});
+app.use(ProdutoRoutes)
 
 //Exporta o aplicativo express configurado
 module.exports = app;
---
+++ app-api/src/api/repositories/database/models/Produto.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class Produto extends Model {
+        static associate(models) {
+            this.belongsTo(models.Loja, {
+                foreignKey: 'idLoja',
+                onDelete: 'CASCADE',
+                onUpdate: 'CASCADE'
+            })
+        }
+    }
+
+    Produto.init({
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        quantidade: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+            allowNull: false
+        },
+        categoria: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        preco: {
+            type: DataTypes.DOUBLE,
+            defaultValue: 0,
+            allowNull: false
+        }
+    }, {
+        sequelize: sequelize,
+        tableName: 'produtos',
+        modelName: 'Produto',
+    });
+
+    return Produto;
+}
